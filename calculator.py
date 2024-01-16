f9dfbbe73488313406d6d63e2934afc6f222a6ac
DESKTOP-TPV8FLK\INSONG <mihwaiuck37@gmail.com>
2024-01-16 20:57:19 +0900

    input 기능 캡슐화

--- calculator.py
+++ calculator.py
@@ -1,17 +1,11 @@
+import input_par
 import multiply
 import division
 import add_sub
 
 def start():
     
-    input_str = input("정수, 기호, 정수를 입력하세요 (예: 5 + 3): ") 
-
-    # 입력된 문자열을 공백을 기준으로 분리
-    num1, symbol, num2 = input_str.split()
-
-    # 분리된 문자열을 각각 정수와 문자열로 변환
-    num1 = int(num1)
-    num2 = int(num2)
+    num1, symbol, num2 = input_par.input_2par_1symbol()
 
     if symbol == '*':
         print(multiply.mul(num1,num2),"입니다")
